67352f91796e14b5da635c5b0cc9dc493e8804d1
Piotr Osmola <piotr.osmola@gmail.com>
2023-11-19 21:48:37 +0100

    Add errror page

---
+++ error.php
@@ -0,0 +1,41 @@
+  <!-- Header -->
+  <?php include 'header.php'; ?>
+
+  <!-- Section - Hero Error -->
+  <section class="section hero-error">
+    <!-- Background -->
+    <div class="section-bg">
+      <div class="waves reversed ">
+        <div class="waves-inner">
+          <video src="assets/video/waves.mp4" autoplay loop muted playsinline></video>
+        </div>
+      </div>
+    </div>
+    <!-- Container -->
+    <div class="section-container container">
+      <div class="container-inner">
+        <!-- Content -->
+        <div class="section-content fullwidth">
+          <div class="row justify-content-center">
+            <div class="col-lg-11">
+              <div class="row">
+                <div class="col-lg-7">
+                  <h1 class="outline-text mb-0">404</h1>
+                  <h5>It Seems That You're Lost...</h5>
+                </div>
+                <div class="col-md-5 ml-auto text-md-right">
+                  <!-- Button -->
+                  <a href="#" class="btn btn-oval" data-mp>
+                    <span class="btn-bg" data-mp-item data-mp-scale="1.15"></span>
+                    <span class="btn-outline" data-mp-item="0.05" data-mp-scale="1.075"></span>
+                    <i class="btn-icon i i-play"></i>
+                    <span class="btn-label">Go Home</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
